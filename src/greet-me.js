bb58113e0d9186d2e9227d170421589ba86e2044
Omar Griffin <griffin.omar@gmail.com>
2015-04-05 03:51:41 +0000

    A message that is relevant to the changes in my file.

---
+++ src/greet-me.js
@@ -0,0 +1,22 @@
+
+// Here's a fun exercise:
+// Part 1: Write a program that asks the user for his name and greets him with his name.
+// Here's an example of how the program should run:
+//
+// Hey buddy! What's your name?
+// *user inputs his/her name* 
+// Oh wow, *inputted name*! That's such an awesome name. 
+//
+// Hint: Feel free use some code from read-input.js in order to read user input from the console.
+// There's no single correct answer - simply try to find a solution that works!
+
+// Part 2: Feeling ambitious? Modify the program so that it responds differently based on the name entered.
+// For example:
+//
+// Hey buddy! What's your name?
+// *user inputs his/her name*
+// Oh wow, *name*! That's such an awesome name.
+
+// Hey buddy! What's your name?
+// *user inputs his/her name*
+// Meh, *name* is an okay name I guess.
